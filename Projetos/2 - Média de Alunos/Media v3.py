5316af5379aa6f3c0eaa1d4b4b49a79327decf1d
André Luis <andredevdantas@gmail.com>
2024-05-18 12:01:24 -0300

    Atualização projeto 2

--- Projetos/2 - Média de Alunos/Media v3.py
+++ Projetos/2 - Média de Alunos/Media v3.py
@@ -2,12 +2,14 @@
 
 n = int(input("Informe o número de alunos: "))
 
+medias = []
 controle = 1
 while controle <= n:
     n1 = float(input(f"Informe a primeira nota do aluno {controle}: "))
     n2 = float(input(f"Informe a segunda nota do aluno {controle}: "))
 
     media = round((n1 + n2) / 2,1)
+    medias.append((controle, media)) 
 
     if media >= 6:
         print(f"Aluno {controle} foi aprovado com média {media}")
@@ -15,3 +17,7 @@ while controle <= n:
         print(f"Infelizmente o aluno {controle} foi reprovado com média {media}")
     
     controle += 1
+
+print("\nMédia dos Alunos: ")
+for aluno, media in medias:
+    print(f"Aluno {aluno}: {media}")
