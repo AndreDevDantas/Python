79eaf567a07d8896acac580a2d81d50ef6f22c86
André Luis <andredevdantas@gmail.com>
2024-05-21 19:47:55 -0300

    testes com Tkinter 2

---
+++ Estudos/4 - Tkinter/teste 2.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+janela = tk.Tk()
+janela.title("Jokenpo")
+label = tk.Label(janela, text = 'Exemplo Rótulo', background = 'black', foreground = 'white')
+
+#Posso abreviar
+label2 = tk.Label (janela, text = 'Exemplo 2', bg = 'blue', fg = 'red')
+
+
+
+label.pack()
+label2.pack()
+janela.mainloop()
