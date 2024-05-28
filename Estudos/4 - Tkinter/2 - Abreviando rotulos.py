06237d07d82365348e6fdfe8cbed93b504f8e85b
André Luis <andredevdantas@gmail.com>
2024-05-28 19:57:45 -0300

    Avançando em estudos tkinter

--- Estudos/4 - Tkinter/2 - Abreviando rotulos.py
+++ Estudos/4 - Tkinter/2 - Abreviando rotulos.py
@@ -3,10 +3,8 @@ janela = tk.Tk()
 janela.title("Jokenpo")
 label = tk.Label(janela, text = 'Exemplo Rótulo', background = 'black', foreground = 'white')
 
-#Posso abreviar
+#Abreviando
 label2 = tk.Label (janela, text = 'Exemplo 2', bg = 'blue', fg = 'red')
-
-
 
 label.pack()
 label2.pack()
