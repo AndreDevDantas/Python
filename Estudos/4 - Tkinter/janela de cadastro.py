1c2fcde03f7d546c24d5fb05cc48c16ad7f0d14e
André Luis <andredevdantas@gmail.com>
2024-05-28 19:42:02 -0300

    Janela de cadastro finalizada

---
+++ Estudos/4 - Tkinter/janela de cadastro.py
@@ -0,0 +1,22 @@
+import tkinter
+from tkinter import ttk
+
+#Instanciar a janela
+janela = tkinter.Tk()
+
+#declaração de variaveis
+nome, telefone = tkinter.StringVar(), tkinter.StringVar()
+
+def button_click():
+    print(f"nome: {nome.get()}, telefone: {telefone.get()}")
+
+ttk.Label(janela, text="Nome").grid(column=10, row=10)
+ttk.Entry(janela, textvariable=nome).grid(column=20, row=10)
+ttk.Label(janela, text="Telefone").grid(column=10, row=20)
+ttk.Entry(janela, textvariable=telefone).grid(column=20, row=20)
+ttk.Button(janela, text="Inserir", command=button_click).grid(column=20, row=30)
+
+janela.title("Container")
+janela.resizable(True, False)
+
+janela.mainloop()
